d8492810a4d165495516e6f6c7b13096fd057b30
Christopher Luvumbo <christopherluvumbo@gmail.com>
2022-01-18 16:30:49 +0100

    type basics

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
-var character = 'Christopher';
-console.log(character);
-var inputs = document.querySelectorAll('input');
-inputs.forEach(function (input) {
-    console.log(input);
-});
+/**
+ * No es obligatorioindicar que tipo son porque typescript
+ * lo reconoce directamente
+ */
+var person = 'Chrsitopher';
+var age = 23;
+/*
+* En el parametro que pasamos indicamos que es de tipo numero
+* sino no va a compilarse
+*/
+var monthsCalc = function (year) { return year * 12; };
+/**
+ * Generaria un error ya que 'Helo es un String y esta indicado que el parametro
+ * a recibir tiene que ser un numero por lo qe no se compilara
+ */
+//console.log(circ('hello'));
+console.log(monthsCalc(23));
--- app.ts
+++ app.ts
@@ -1,6 +1,21 @@
-const character = 'Christopher';
-console.log(character);
-const inputs = document.querySelectorAll('input');
-inputs.forEach(function (input) {
-    console.log(input);
-});
+/**
+ * No es obligatorioindicar que tipo son porque typescript
+ * lo reconoce directamente
+ */
+
+let person: String = 'Chrsitopher';
+let age: Number = 23;
+
+/*
+* En el parametro que pasamos indicamos que es de tipo numero
+* sino no va a compilarse
+*/ 
+const monthsCalc = (year: number) => year * 12;
+/**
+ * Generaria un error ya que 'Helo es un String y esta indicado que el parametro
+ * a recibir tiene que ser un numero por lo qe no se compilara
+ */
+
+//console.log(circ('hello'));
+
+console.log(monthsCalc(23));
