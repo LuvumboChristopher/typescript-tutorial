944a102eda0217062c52f8456c3167490a34a32b
Christopher Luvumbo <christopherluvumbo@gmail.com>
2022-01-19 16:21:57 +0100

    function signatures

--- public/app.js
+++ public/app.js
@@ -1,14 +1,17 @@
 "use strict";
-let city;
-city = 69100;
-let age = 'Twentyfive';
-const sayHello = (person) => {
-    const { name, age, country } = person;
-    console.log(`Hey! my name is ${name} i'm from ${country} and i'm ${age} old`);
+//Function signature
+/*Le decimos a la funcion a que se tiene que parecer y en el caso en el que no soga los mismos patrones
+nos enviaria un erro */
+//Ejemplo 1
+let greet;
+greet = (name, greeting) => {
+    console.log(`${name} says ${greeting}`);
 };
-let me = {
-    name: 'Christopher',
-    age: 25,
-    country: 'Spain'
-};
-sayHello(me);
+//Ejemplo 2
+let calc;
+/*
+calc = (n1: number, n2: string): number {
+    return n1 + n2;
+}
+*/
+//No sigue el atron por lo que resulta en erro ya que es segunpo parametro es de tipo string
--- src/app.ts
+++ src/app.ts
@@ -1,31 +1,21 @@
-// Para evitar repetir codigo una y otra vez podemos creamos nuestros propios tipos
-type StringOrNum = string | number;
+//Function signature
+/*Le decimos a la funcion a que se tiene que parecer y en el caso en el que no soga los mismos patrones
+nos enviaria un erro */
 
-let city: StringOrNum;
+//Ejemplo 1
+let greet:(a: string, b: string) => void;
 
-city = 69100;
-
-let age: StringOrNum = 'Twentyfive';
-
-//Creamos el tipo una vez y lo utilizamos repetidas veces en el codigo
-
-// igual ocuree con los objetos
-
-type objectName = {
-    name: string,
-    age: number,
-    country: string,
+greet = (name: string, greeting: string) => {
+    console.log(`${name} says ${greeting}`)
 };
 
-const sayHello = (person: objectName) => {
-    const {name, age, country} = person;
-    console.log(`Hey! my name is ${name} i'm from ${country} and i'm ${age} old`)
-}
+//Ejemplo 2
+let calc: (a: number, b: number) => number;
 
-let me = {
-    name: 'Christopher',
-    age: 25,
-    country: 'Spain'
+/*
+calc = (n1: number, n2: string): number {
+    return n1 + n2;
 }
+*/
 
-sayHello(me);
+//No sigue el atron por lo que resulta en erro ya que es segunpo parametro es de tipo string
