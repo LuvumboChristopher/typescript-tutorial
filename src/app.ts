7bb99f550a90fa8c5862bd2ee5caf9d4d51c9aec
Christopher Luvumbo <christopherluvumbo@gmail.com>
2022-01-19 15:33:47 +0100

    tsconfig & workflow

---
+++ src/app.ts
@@ -0,0 +1,9 @@
+//Workflow en ts , creacion de tsconfig usando : tsc --init
+
+/* En tsconfig cambiamos la ruta ROOT , para que cuando compilemos nuestro ts
+El archivo js sea creado en la carpeta src*/
+
+console.log('test');
+
+/* Tener cuidado y anadir las opciones correctas en el tscconfig, para que solo
+los archivos que provienen de la carpeta src puedan ser compilados*/
