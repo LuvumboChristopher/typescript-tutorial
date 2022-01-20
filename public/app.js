d359093a49605e01591140a4a9d3e99aef1a3cc2
Christopher Luvumbo <christopherluvumbo@gmail.com>
2022-01-20 15:22:20 +0100

    Public, private, readonly

--- public/app.js
+++ public/app.js
@@ -1,19 +1,51 @@
 "use strict";
-// Con elemento como ! o ? indicamos a ts que esos elementos si existen ya que el no puede leer en nuestro documento html
-const anchor = document.querySelector('a');
-console.log(anchor === null || anchor === void 0 ? void 0 : anchor.href);
-// En este caso recuperamos el formulario
-const form = document.querySelector('form');
-/* Pero en este tabn por lo tanto ts lo detecta como un elmenento mas y no un formuario
-para evitar eso podemo s haer lo siguiente*/
-const form1 = document.querySelector('.new-item-form');
-const type = document.querySelector('#type');
-const tofrom = document.querySelector('#tofrom');
-const details = document.querySelector('#details');
-const amount = document.querySelector('#amount');
-form.addEventListener('submit', (e) => {
-    e.preventDefault();
-    console.log(type.value, tofrom.value, details.value, 
-    //Uso de valueAsNumber pare recuperar el valor en formato string y no en numero
-    amount.valueAsNumber);
-});
+class Invoice {
+    constructor(client, details, amount) {
+        this.client = client;
+        this.details = details;
+        this.amount = amount;
+    }
+    format() {
+        return `${this.client} owes £${this.amount} for ${this.details}`;
+    }
+}
+const invoiceOne = new Invoice('Christopher', 'Groceries', 23);
+const invoiceTwo = new Invoice('Alberto', 'Tech', 2300);
+console.log(invoiceOne, invoiceTwo);
+/*En este caso concreto imaginemos que qeuremos crear un array en que solo podamos intrucir elemento creados
+con la clasee 'Invoice'*/
+let invoicesArray = [];
+/* Otro elemento a tener en cuenta en el uso de methodos y propiedades privadas o publicas
+por defecto todos los elementos son publicos pero se pueden volver privados lo que hace que
+no se pueda accedes desde el exterior de la clase */
+class Dog {
+    constructor(race, age, hairColor) {
+        this.race = race;
+        this.age = age;
+        this.hairColor = hairColor;
+    }
+    format() {
+        return `This dog is a ${this.race} with ${this.hairColor} hair,  who is ${this.age}old`;
+    }
+}
+//Esto me daria un error ya que he indicado que la raza es una propiedad privada
+//console.log(tobby.race)
+const tobby = new Dog('Bulldog', 2, 'Brown');
+// Esto tambien ya que le indicado 'Solo lectura' y aqui estoy intentando modificarla
+//tobby.hairColor = 'black';
+// Otra manera de declara una clase en TS
+/*Podemos incluir los elementos directamente en el constructor pero hay que indicar su visibilidad:
+PublicKeyCredential, provada o solo de lectura*/
+class Car {
+    constructor(country, year, brand, city) {
+        this.country = country;
+        this.year = year;
+        this.brand = brand;
+        this.city = city;
+    }
+    format() {
+        return `${this.brand} is a car brand ${this.country} founded in ${this.city} in ${this.year} `;
+    }
+}
+const MB = new Car('Germany', 1926, 'Mercedes-Benz', 'Stuttgart');
+console.log(MB.format());
--- src/app.ts
+++ src/app.ts
@@ -1,31 +1,73 @@
-// Con elemento como ! o ? indicamos a ts que esos elementos si existen ya que el no puede leer en nuestro documento html
+class Invoice {
+    client: string;
+    details: string;
+    amount: number;
 
-const anchor = document.querySelector('a');
-console.log(anchor?.href);
+    constructor(client: string, details: string, amount: number){
+        this.client = client;
+        this.details = details;
+        this.amount = amount;
+    }
 
+    format(){
+        return `${this.client} owes £${this.amount} for ${this.details}`;
+    }    
+}
 
-// En este caso recuperamos el formulario
-const form = document.querySelector('form')!;
+const invoiceOne = new Invoice('Christopher','Groceries',23);   
+const invoiceTwo = new Invoice('Alberto','Tech',2300);   
 
-/* Pero en este tabn por lo tanto ts lo detecta como un elmenento mas y no un formuario
-para evitar eso podemo s haer lo siguiente*/
+console.log(invoiceOne,invoiceTwo);
 
-const form1 = document.querySelector('.new-item-form') as HTMLFormElement;
+/*En este caso concreto imaginemos que qeuremos crear un array en que solo podamos intrucir elemento creados
+con la clasee 'Invoice'*/
 
+let invoicesArray: Invoice[] = []
 
-const type = document.querySelector('#type') as HTMLInputElement;
-const tofrom = document.querySelector('#tofrom') as HTMLInputElement;
-const details = document.querySelector('#details') as HTMLInputElement;
-const amount = document.querySelector('#amount') as HTMLInputElement;
+/* Otro elemento a tener en cuenta en el uso de methodos y propiedades privadas o publicas
+por defecto todos los elementos son publicos pero se pueden volver privados lo que hace que 
+no se pueda accedes desde el exterior de la clase */
 
-form.addEventListener('submit', (e: Event) => {
-  e.preventDefault();
+class Dog {
+    private race: string;
+    public age: number;
+    readonly hairColor: string;
 
-  console.log(
-    type.value, 
-    tofrom.value, 
-    details.value, 
-    //Uso de valueAsNumber pare recuperar el valor en formato string y no en numero
-    amount.valueAsNumber
-  );
-});
+    constructor(race: string, age: number, hairColor: string){
+        this.race = race;
+        this.age = age;
+        this.hairColor = hairColor;
+    }
+
+    format(){
+        return `This dog is a ${this.race} with ${this.hairColor} hair,  who is ${this.age}old`;
+    }    
+}
+
+//Esto me daria un error ya que he indicado que la raza es una propiedad privada
+//console.log(tobby.race)
+
+const tobby = new Dog('Bulldog',2,'Brown');
+
+// Esto tambien ya que le indicado 'Solo lectura' y aqui estoy intentando modificarla
+//tobby.hairColor = 'black';
+ 
+
+// Otra manera de declara una clase en TS
+/*Podemos incluir los elementos directamente en el constructor pero hay que indicar su visibilidad: 
+PublicKeyCredential, provada o solo de lectura*/
+class Car {
+    constructor(
+        public country: string,
+        public year: number,
+        public brand: string,
+        public city: string,
+    ){}
+
+    format(){
+        return `${this.brand} is a car brand ${this.country} founded in ${this.city} in ${this.year} `;
+    }    
+}
+
+const MB = new Car('Germany',1926,'Mercedes-Benz', 'Stuttgart');
+console.log(MB.format())
